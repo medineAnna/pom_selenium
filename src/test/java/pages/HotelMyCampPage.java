530c8ccdc556f1984d1393f4ca52f8c154aadb92
medineAnna <medine.ah>
2022-02-04 22:18:14 +0100

    webTable

--- src/test/java/pages/HotelMyCampPage.java
+++ src/test/java/pages/HotelMyCampPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class HotelMyCampPage {
@@ -28,11 +29,36 @@ public class HotelMyCampPage {
     @FindBy(xpath="//span[text()='ListOfUsers']")
     public WebElement basariliGirisYaziElementi;
 
+    @FindBy(xpath = "//span[text()='Hotel Management']")
+    public WebElement hotelManagementLink;
+
+    @FindBy(xpath = "//a[@href='/admin/HotelAdmin']")
+    public WebElement hotelListLink;
+
+    @FindBy(xpath = "//a[@class='btn btn-circle btn-default']")
+    public WebElement addHotelLink;
+
+    @FindBy(xpath = "//input[@id='Code']")
+    public WebElement addHotelCodeKutusu;
+
+    @FindBy ( xpath="//select[@id='IDGroup']")
+    public WebElement addHotelDropDown;
+
+    @FindBy(xpath = "//button[@id='btnSubmit']")
+    public WebElement addHotelSaveButton;
+
     public void bekle(int saniye){
         try {
             Thread.sleep(saniye*1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    public void girisYap(){
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        ilkLoginLink.click();
+        usernameBox.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
+        passwordBox.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
+        loginButtonu.click();
     }
 }
---
+++ src/test/java/smokeTest/NegativeLoginTest.java
@@ -0,0 +1,53 @@
+package smokeTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HotelMyCampPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class NegativeLoginTest {
+    static HotelMyCampPage hotelMyCampPage;
+    @Test
+    public void yanlisSifreTesti(){
+        //1) smokeTest  paketi altinda yeni bir Class olustur: NegativeTest
+        //3 Farkli test Methodunda 3 senaryoyu test et
+        //                - yanlisSifre
+        //                - yanlisKulllanici
+        //                - yanlisSifreKullanici
+        //test data yanlis username: manager1 , yanlis password : manager1
+        //2) https://www.hotelmycamp.com adresine git
+        //3) Login butonuna bas
+        //4) Verilen senaryolar ile giris yapilamadigini test et
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        hotelMyCampPage=new HotelMyCampPage();
+        hotelMyCampPage.ilkLoginLink.click();
+        hotelMyCampPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
+        hotelMyCampPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCWrongPassword"));
+        hotelMyCampPage.loginButtonu.click();
+        Assert.assertTrue(hotelMyCampPage.girisYapilamadiYazisi.isDisplayed());
+        Driver.closeDriver();
+    }
+    @Test
+    public void yalnisUsernameTesti(){
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        hotelMyCampPage=new HotelMyCampPage();
+        hotelMyCampPage.ilkLoginLink.click();
+        hotelMyCampPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCWrongUsername"));
+        hotelMyCampPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
+        hotelMyCampPage.loginButtonu.click();
+        Assert.assertTrue(hotelMyCampPage.girisYapilamadiYazisi.isDisplayed());
+        Driver.closeDriver();
+    }
+    @Test
+    public void yalnisSifreKullaniciTesti(){
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        hotelMyCampPage=new HotelMyCampPage();
+        hotelMyCampPage.ilkLoginLink.click();
+        hotelMyCampPage.usernameBox.sendKeys(ConfigReader.getProperty("HMCWrongUsername"));
+        hotelMyCampPage.passwordBox.sendKeys(ConfigReader.getProperty("HMCWrongPassword"));
+        hotelMyCampPage.loginButtonu.click();
+        Assert.assertTrue(hotelMyCampPage.girisYapilamadiYazisi.isDisplayed());
+        Driver.closeDriver();
+    }
+}
